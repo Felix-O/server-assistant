fdf88a307cfb270d0a5dbb0bc10702921d6416f9
Felix-O <felix.oje@gmail.com>
2020-11-02 22:23:04 +0000

    nodemailer setup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,45 @@
 import * as functions from 'firebase-functions';
+const nodemailer = require('nodemailer');
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.pubsub.schedule('0 8 * * 3').onRun(async (context) => {
+    
+    const text = `<div>
+      <p>${"Could God's Extended Hand get 30 boxes this week? Thank you much!"}</p>
+    </div>`;
+
+    const auth = functions.config().nodemailer;
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            type: 'OAuth2',
+            user: auth.user,
+            clientId: auth.client_id,
+            clientSecret: auth.client_secret,
+            accessToken: auth.access_token,
+            refreshToken: auth.refresh_token,
+        }
+    });
+
+    const mailOptions = {
+        to: "tyoung@cityviewsd.com",
+        subject: `Food boxes`,
+        // text: text,
+        html: text
+    };
+ 
+    return transporter.sendMail(mailOptions)
+    .then((info: any) => console.log('Email sent: ' + info.response))
+    .catch((error: any) => console.log("Error sending email ---- ", error.message));
+
+    // return null;
+    
+  });
